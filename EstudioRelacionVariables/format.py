ab3592c208b429885548a73b3fb72ebd715951ab
Natalia Castejon <natt.cf@gmail.com>
2019-10-13 23:54:26 +0200

    mas formateo de columnas y primer inicio de README

--- EstudioRelacionVariables/format.py
+++ EstudioRelacionVariables/format.py
@@ -20,7 +20,7 @@ fields=['Chr','Start','Ref','Alt_Annovar','Alt_IR','avsnp147','genotype','maf','
         'Polyphen2_HVAR_score','polyphen',
         'Func.refGene','Func.ensGene','ExonicFunc.refGene','ExonicFunc.ensGene','MutationTaster_score',
         'PROVEAN_score','CADD_phred','phyloP20way_mammalian',
-        'SiPhy_29way_logOdds','FATHMM_score','FATHMM_pred','CLNREVSTAT','CLNSIG','allele_coverage','function',
+        'SiPhy_29way_logOdds','FATHMM_score','CLNSIG','allele_coverage','function',
         'grantham','5000Exomes','FATHMM','clinvar','phylop','gnomAD_genome_ALL','1000g2015aug_all','1000G_ALL',
         'ExAC_ALL','AF','AF_male','AF_female','PopFreqMax','alelos']
 
@@ -76,7 +76,13 @@ def categoria_por_valor (linea,dic):
 
 ###
 ### -- Lectura del archivo de variantes
-variantes_DF=pd.read_csv(path , sep='\t' , header=header,dtype={'causal': 'bool','MutationTaster_score':'float'},decimal=',', usecols=fields,low_memory=False)
+variantes_DF=pd.read_csv(path , sep='\t', 
+                         header=header,dtype={'causal': 'bool','MutationTaster_score':'float','SIFT_score':'float','Polyphen2_HDIV_score':'float',
+                                              'Polyphen2_HVAR_score':'float','MutationTaster_score':'float','PROVEAN_score':'float',
+                                              'CADD_phred':'float','phyloP20way_mammalian':'float','SiPhy_29way_logOdds':'float',
+                                              'FATHMM_score':'float','gnomAD_genome_ALL':'float','1000g2015aug_all':'float','1000G_ALL':'float',
+                                              'ExAC_ALL':'float','AF':'float','AF_male':'float','AF_female':'float','PopFreqMax':'float','alelos':'float'},
+                                              decimal=',', usecols=fields,low_memory=False)
 
 
 ###
@@ -91,7 +97,7 @@ del(ser)
 
 # Obtenemos el primer numero como el valor del predictor y casteamos a float
 variantes_DF=variantes_DF.join(variantes_DF.pop('ljb23_sift').str.split(',', expand=True))
-variantes_DF=variantes_DF.rename(columns={ 0:'ljb23_sift',  1:'resto'})
+variantes_DF=variantes_DF.rename(columns={ 0:'ljb23_sift'})
 variantes_DF['ljb23_sift']=variantes_DF['ljb23_sift'].astype(float)
 
 # Eliminamos los | del principio y el final y celda a celda buscamos formato correspondiente: 
@@ -111,7 +117,7 @@ ser=[]
 variantes_DF['polyphen']=pd.to_numeric(ser)
 del(ser)
 
-# Transformamos valores str en numericos según hoja equivalencias.txt.
+# Transformamos valores str en numericos segun tabla de equivalencias.
 # Cuando haya dos campos juntos afectando una variante, se toma el campo de mayor grado
 dic1={'exonic':1.,'splicing':1.,'ncRNA':2.,'UTR5':3.,'UTR3':3.,'intronic':4.,'upstream':5.,'downstream':5.,'intergenic':6.}
 
@@ -148,22 +154,18 @@ del variantes_DF['ExonicFunc.ensGene']
 
 
 
-#phyloP20way_mammalian    float64
-#SiPhy_29way_logOdds      float64
-#FATHMM_score             float64
-#FATHMM_pred               object
-#CLNREVSTAT                object
-#CLNSIG                    object
+
+#CLNSIG                    object   cambiar a ordinal
 #allele_coverage           object
-#function                  object
-#grantham                  object
-#5000Exomes                object
-#FATHMM                    object
-#clinvar                   object
-#phylop                    object
+#function                  object   cambiar a ordinal
+#grantham                  object   buscar info sobre significado de distancia grantham (max o min?)
+#5000Exomes                object   buscar AMAF GMMAF EMAF
+#FATHMM                    object   buscar info de significado
+#clinvar                   object   cambiar a ordinal
+#phylop                    object   buscar info de significado
 
 
-print(variantes_DF['CADD_pred'].unique())
+#print(variantes_DF['phylop'].unique())
 
 #print(variantes_DF.dtypes)
 
--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # datascience_tfm
-Final master project to DataScience master degree
+
+Currently work have been presented as the final project to DataScience master degree.
+
+
+##### Table of contents
+* [Manual pages](#manual)
+* [Some considerations before start](#before)
+* [Installation and requirements](#install)
+* [Analisys of the relation between variables](#relation)
+* [Clustering](#cluster)
+* [Final prioritization](#prioritization)
+
+---
+<a name="manual"></a>
+### Manual pages
+
+Documentation for the project utilities is abailable by cloning this repository.
+
+---
+<a name="install"></a>
+### Installation and requirements
+The project requires:
+
+  * **Linux system** (>= Ubuntu 18.04 LTS system recommended; no other distributions were tested)
+  * **Python** (>=3.7 recommended). Python languaje can be easily installed throug the [anaconda environment] (https://www.anaconda.com/distribution/).
+
+The remaining dependencies can be installed using the included `setup.sh` script. 
+Basic instructions:
+```bash
+git clone https://github.com/nacasfer/datascience_tfm
+cd datascience_tfm
+bash setup.sh
+```
+The installer may also be used to check for updates to this and co-dependent packages.
+
+---
+<a name="before"></a>
+## Some considerations before start
+
+Currently project starts with a set of genomic variants. These variants where collected by our group and no distribution is available. Random example using [1000genomes] (https://)  phase 3 database is provided for script testing.
+Aligment, varint calling and variant VCF annotation steps were perform with following modules using UCSC [HG37] fasta reference and a proper BED file:
+
+  1. Mapping step was perform using [BWA] (http://) software
+  2. Variant Call step was perform folllowing best practices of [GATK](http://) through `--HaploTypeCaller` module.
+  3. Variant annotation step was perform by joint output from two annotation workflows: [Annovar] (https://) and [IonReporter](https://) web service.
+  4. Final datasets were obtained by joint of manualy-anotated tsv patient files for the dissease variant candidate. 
+
+
+---
+<a name="relation"></a>
+## Analysis of the relation between variables
+
+
+---
+<a name="clustering"></a>
+## Clustering
+
+
+---
+
+<a name="prioritization"></a>
+## Final prioritization
+
