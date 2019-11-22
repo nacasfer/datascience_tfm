5b292e6e66dbd8e5ba3eed2cf68ead8e813a5615
natalia <natt.cf@gmail.com>
2019-11-22 12:25:58 +0100

    Cambio de nombre de la carpeta EstudioRelacionVariables por Entrenamiento

--- predict.py
+++ predict.py
@@ -4,11 +4,27 @@
 @author: natalia castejon fernandez
 """
 
-
-
 ## --  Modulos -- ##
 
+import pandas as pd
 import pickle
+import dataformat as dfmt
+
+
+
+
+## --  Parametros globales -- ##
+
+path='/media/natalia/DATOS/DATA/Natalia/ML/DATOS/csv/12810.txt'
+header=0
+fields=['Chr','Start','Ref','Alt_Annovar','Alt_IR','avsnp147','genotype','maf','gene',
+        'causal','SIFT_score','ljb23_sift','sift','Polyphen2_HDIV_score',
+        'Polyphen2_HVAR_score','polyphen',
+        'Func.refGene','Func.ensGene','ExonicFunc.refGene','ExonicFunc.ensGene','MutationTaster_score',
+        'PROVEAN_score','CADD_phred','phyloP20way_mammalian',
+        'SiPhy_29way_logOdds','FATHMM_score','CLNSIG','allele_coverage','function',
+        'grantham','5000Exomes','FATHMM','clinvar','phylop','gnomAD_genome_ALL','1000g2015aug_all','1000G_ALL',
+        'ExAC_ALL','AF','PopFreqMax','alelos']
 
 
 
@@ -16,8 +32,34 @@ import pickle
 
 ## --  Funciones -- ##
 
-def aplica_modelos_fillNaN(X_test,Y_test):
-    filename=''
-    loaded_model = pickle.load(open(filename, 'rb'))
-    result = loaded_model.score(X_test, Y_test)
-    print(result)
+#def aplica_modelos_fillNaN(X_test,Y_test):
+#    filename=''
+#    loaded_model = pickle.load(open(filename, 'rb'))
+#    result = loaded_model.score(X_test, Y_test)
+#    print(result)
+
+
+
+
+###
+### -- Lectura del archivo de variantes
+variantes_DF=pd.read_csv(path , sep='\t', 
+                             header=header,
+                             dtype={'causal': 'bool',
+                                    'SIFT_score':'float',
+                                    'Polyphen2_HDIV_score':'float','Polyphen2_HVAR_score':'float',
+                                    'MutationTaster_score':'float','PROVEAN_score':'float','CADD_phred':'float',
+                                    'phyloP20way_mammalian':'float',
+                                    'SiPhy_29way_logOdds':'float',
+                                    'FATHMM_score':'float',
+                                    'gnomAD_genome_ALL':'float','1000g2015aug_all':'float','1000G_ALL':'float',
+                                    'ExAC_ALL':'float','AF':'float','PopFreqMax':'float','alelos':'float'},
+                                     decimal=',', usecols=fields,low_memory=False)
+
+
+###
+### -- Formateo de los datos
+print ('Data format processing')
+variantes_DF=dfmt.formato_datos_origen(variantes_DF)
+
+print(list(variantes_DF))
