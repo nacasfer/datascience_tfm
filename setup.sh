7f255655ea2cbc8485f47e8cdb329228a6817049
nacasfer <38558227+nacasfer@users.noreply.github.com>
2019-12-12 16:55:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 sudo apt install python3-pip
 sudo python3 -m pip install numpy scipy matplotlib ipython jupyter pandas sympy nose imblearn
 sudo python3 -m pip instal pickle
+sudo pip3 install glob3
